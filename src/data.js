df5b081df1b6d975f0c69f77048acd7cac383759
Yingjie Wang <74107605+cato-o@users.noreply.github.com>
2024-01-19 09:35:55 +0800

    reordering and website title fix

--- src/data.js
+++ src/data.js
@@ -9,6 +9,42 @@ export const projects = [
     link: "https://www.cs.columbia.edu/~chilton/",
   },
   {
+    title: "VR Training Simulation Game",
+    subtitle: "Pizza Time",
+    description:
+      "Built voice activated teleport navigation in hand-tracked VR pizza making training simulator for Meta Quest3, allowing users flexible, hands-free movement controls.",
+    image: "./images/pizzaTime.png",
+    mediaType: "image",
+    link: "https://youtu.be/hsBdzE6WyjQ",
+  },
+  {
+    title: "Coral",
+    subtitle: "HackDuke Winner: Education Track (2020)",
+    description:
+      "Built front end user interface for a Chrome extension that translates select vocabulary inline to cultivate immersive foreign language learning during daily Internet browsing.",
+    image: "./images/coral.png",
+    mediaType: "image",
+    link: "https://devpost.com/software/coral",
+  },
+  {
+    title: "moonrabbit",
+    subtitle: "monoprint, projection, 2D digital animation",
+    description:
+      "Multimedia printmaking installation project. 2D digital figure and scene animation is projected onto physical monoprinted background scrolls, bridging digital and physical to build a space that transcends remembrance and memory.",
+    image: "./images/scrollAnim.mp4",
+    mediaType: "video",
+    link: "",
+  },
+  {
+    title: "Illustrator",
+    subtitle: "Columbia Daily Spectator",
+    description:
+      "Illustrated numerous articles for Columbia's student-run newspaper, serving in a variety of leadership roles including as the Head of the Illustrations team.",
+    image: "./images/campus.gif",
+    mediaType: "gif",
+    link: "https://www.columbiaspectator.com/contributors/Yingjie-Wang/",
+  },
+  {
     title: "General Technical Director Intern",
     subtitle: "Walt Disney Animation Studios",
     description:
@@ -27,15 +63,6 @@ export const projects = [
     link: "https://youtu.be/AnO-IiImLko",
   },
   {
-    title: "VR Training Simulation Game",
-    subtitle: "Pizza Time",
-    description:
-      "Built voice activated teleport navigation in hand-tracked VR pizza making training simulator for Meta Quest3, allowing users flexible, hands-free movement controls.",
-    image: "./images/pizzaTime.png",
-    mediaType: "image",
-    link: "https://youtu.be/hsBdzE6WyjQ",
-  },
-  {
     title: "2D Unity Tower Defense Game",
     subtitle: "Gnome Garden Defense",
     description:
@@ -43,51 +70,30 @@ export const projects = [
     image: "./images/gnome.png",
     mediaType: "image",
     link: "https://youtu.be/ShAKwjwg5Co",
-  },
-  {
-    title: "Coral",
-    subtitle: "HackDuke Winner: Education Track (2020)",
-    description:
-      "Built front end user interface for a Chrome extension that translates select vocabulary inline to cultivate immersive foreign language learning during daily Internet browsing.",
-    image: "./images/coral.png",
-    mediaType: "image",
-    link: "https://devpost.com/software/coral",
-  },
-  {
-    title: "Illustrator",
-    subtitle: "Columbia Daily Spectator",
-    description:
-      "Illustrated numerous articles for Columbia's student-run newspaper, serving in a variety of leadership roles including as the Head of the Illustrations team.",
-    image: "./images/campus.gif",
-    mediaType: "gif",
-    link: "https://www.columbiaspectator.com/contributors/Yingjie-Wang/",
-  },
-  {
-    title: "moonrabbit",
-    subtitle: "monoprint, projection, 2D digital animation",
-    description:
-      "Multimedia prinmaking installation project. 2D digital figure and scene animation is projected onto physical monoprinted background scrolls, bridging digital and physical to build a space that transcends remembrance and memory.",
-    image: "./images/scrollAnim.mp4",
-    mediaType: "video",
-    link: "",
   }
 ];
 
-export const testimonials = [
+export const job = [
   {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
+    role: "General Technical Director Intern",
+    company: "Walt Disney Animation Studios",
+    dates: "May-August 2023",
+    description:
+      "Pioneered a data scraper extracting crowd diversity metrics from USD (Universal Scene Description) data on feature films. Developed full stack web data viewport features, integrating data visualizations into dashboard utilized by key stakeholders and production executives.",
+    image: "./images/wdas.jpg",
+    mediaType: "image",
+    skills: "Python, USD (Universal Scene Description), 3D Animation Pipeline, React, Plotly, d3.js"
   },
   {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
-  },
+    role: "Summer Technology Analyst",
+    company: "Federal Reserve Bank of New York",
+    dates: "June-July 2022",
+    description:
+      "Analyzed sentiment dispersion trends and keyword phrasing across 20 years of communications data using Python (Pandas, Plotly), SQL, and Natural Language Processing (NLP) models, collaborating with Markets and Tech teams. Uncovered gaps in training data by creating data visualizations that led to expedited NLP model improvements.",
+    image: "./images/research.png",
+    mediaType: "image",
+    skills: "Python, SQL, NLP, React, Plotly"
+  }
 ];
 
 export const skills = [
